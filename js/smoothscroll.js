eabbc3ac2a1f021297ed5cfc94332f10b66a789a
Crickshaw <marchesedj4@gmail.com>
2018-10-06 18:34:34 -0400

    Fixed Smooth Scroll, updated responsive design on modal

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,34 +1,40 @@
-$(".navbar-collapse ul li a[href='#']").on('click', function(e) {
+$(document).ready(function() {
+	$('.navbar-collapse ul li a[href*=#]:not([href=#])').on('click', function(event) {
 
+	// Make sure this.hash has a value 
+	// before overriding default behavior
+	if(this.hash !== "") {
+	
 	// prevent default anchor click behavior
-	e.preventDefault();
+	event.preventDefault();
 
 	// store hash
 	var hash = this.hash;
 
 	// animate
 	$('html, body').animate({
-		scrollTop: $(this.hash).offset().top -50
-	}, 700, function() {
+		scrollTop: $(hash).offset().top
+		}, 700, function() {
 
 		// when done, add hash to url
 		// (default click behavior)
 		window.location.hash = hash;
+			});
+		} // End if
 	});
-
 });
 
-$("#back-to-top a[href='#']").on('click', function(e) {
+$("#back-to-top a[href='#']").on('click', function(event) {
 
 	// prevent default anchor click behavior
-	e.preventDefault();
+	event.preventDefault();
 
 	// store hash
 	var hash = this.hash;
 
 	// animate
 	$('html, body').animate({
-		scrollTop: $(this.hash).offset().top -50
+		scrollTop: $(hash).offset().top -50
 	}, 700, function() {
 
 		// when done, add hash to url
